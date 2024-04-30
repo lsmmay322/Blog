4178c97657cb5ccec09d1da8d8fcaeb61e8ce21b
hwanseob.lee <lsmmay322@naver.com>
2024-04-30 21:21:30 +0900

    MySQL 연동

---
+++ hwanseob/src/main/java/com/blog/hwanseob/HwanseobApplication.java
@@ -0,0 +1,13 @@
+package com.blog.hwanseob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwanseobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwanseobApplication.class, args);
+	}
+
+}
